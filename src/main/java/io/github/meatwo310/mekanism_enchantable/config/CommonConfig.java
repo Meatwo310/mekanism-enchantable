4c90f1156a0bd3141dcb9fcdc715245c5f376af7
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2024-11-23 00:02:53 +0900

    追加: 原子分解機のエンチャントに対応

--- src/main/java/io/github/meatwo310/mekanism_enchantable/config/CommonConfig.java
+++ src/main/java/io/github/meatwo310/mekanism_enchantable/config/CommonConfig.java
@@ -8,22 +8,32 @@ import net.minecraftforge.fml.common.Mod;
 public class CommonConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
+    // Meka-Tool
     public static final ForgeConfigSpec.BooleanValue MEKATOOL_ENCHANTABLE = BUILDER
             .comment("Whether to allow enchanting of Meka-Tool")
             .define("mekaToolEnchantable", true);
-
     public static final ForgeConfigSpec.IntValue MEKATOOL_ENCHANTABILITY = BUILDER
             .comment("Enchantability of Meka-Tool. Higher values mean better enchantments")
             .defineInRange("mekaToolEnchantability", 15, 1, Integer.MAX_VALUE);
-
     public static final ForgeConfigSpec.BooleanValue MEKATOOL_ALLOW_ALL_ENCHANTMENTS = BUILDER
             .comment("Whether to allow ALL enchantments on Meka-Tool. Set false to limit to tools and weapons enchantments")
             .define("mekaToolAllowAllEnchantments", false);
 
+    // Atomic Disassembler
+    public static final ForgeConfigSpec.BooleanValue ATOMIC_DISASSEMBLER_ENCHANTABLE = BUILDER
+            .comment("Whether to allow enchanting of Atomic Disassembler")
+            .define("atomicDisassemblerEnchantable", true);
+    public static final ForgeConfigSpec.IntValue ATOMIC_DISASSEMBLER_ENCHANTABILITY = BUILDER
+            .comment("Enchantability of Atomic Disassembler. Higher values mean better enchantments")
+            .defineInRange("atomicDisassemblerEnchantability", 15, 1, Integer.MAX_VALUE);
+    public static final ForgeConfigSpec.BooleanValue ATOMIC_DISASSEMBLER_ALLOW_ALL_ENCHANTMENTS = BUILDER
+            .comment("Whether to allow ALL enchantments on Atomic Disassembler. Set false to limit to tools and weapons enchantments")
+            .define("atomicDisassemblerAllowAllEnchantments", false);
+
+    // Armor
     public static final ForgeConfigSpec.BooleanValue ARMOR_ENCHANTABLE = BUILDER
             .comment("Whether to allow enchanting of armors, such as Meka-Suit, (Armored) Jetpack, (Armored) Free Runners, etc.")
             .define("armorEnchantable", true);
-
     public static final ForgeConfigSpec.IntValue ARMOR_ENCHANTABILITY = BUILDER
             .comment("Enchantability of armors. Higher values mean better enchantments")
             .defineInRange("armorEnchantability", 15, 1, Integer.MAX_VALUE);
---
+++ src/main/java/io/github/meatwo310/mekanism_enchantable/mixin/tool/ItemAtomicDisassemblerMixin.java
@@ -0,0 +1,66 @@
+package io.github.meatwo310.mekanism_enchantable.mixin.tool;
+
+import io.github.meatwo310.mekanism_enchantable.api.IEnchantableItem;
+import io.github.meatwo310.mekanism_enchantable.config.CommonConfig;
+import io.github.meatwo310.mekanism_enchantable.mixin.ItemMixin;
+import mekanism.common.item.gear.ItemAtomicDisassembler;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.Enchantments;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Set;
+
+@Mixin(value = ItemAtomicDisassembler.class, remap = false)
+public class ItemAtomicDisassemblerMixin extends ItemMixin implements IEnchantableItem {
+    @Unique
+    private static final Set<EnchantmentCategory> enchantable_mekatools$ALLOWED = Collections.unmodifiableSet(EnumSet.of(
+            //            EnchantmentCategory.BREAKABLE, // TODO: Support this category
+            EnchantmentCategory.DIGGER,
+            EnchantmentCategory.WEAPON
+    ));
+
+    @Unique
+    private static final Set<Enchantment> enchantable_mekatools$DENIED = Set.of(
+            Enchantments.SWEEPING_EDGE
+    );
+
+    @Override
+    @Inject(method = IEnchantableItem.IS_ENCHANTABLE, at = @At("HEAD"), cancellable = true)
+    public void isEnchantable(@NotNull ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        if (!CommonConfig.ATOMIC_DISASSEMBLER_ENCHANTABLE.get()) return;
+        cir.setReturnValue(true);
+    }
+
+    @Override
+    @Inject(method = IEnchantableItem.IS_BOOK_ENCHANTABLE, at = @At("HEAD"), cancellable = true)
+    public void isBookEnchantable(ItemStack stack, ItemStack book, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+
+    @Override
+    @Inject(method = IEnchantableItem.CAN_APPLY_AT_ENCHANTING_TABLE, at = @At("HEAD"), cancellable = true)
+    public void canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment, CallbackInfoReturnable<Boolean> cir) {
+        if (!CommonConfig.ATOMIC_DISASSEMBLER_ENCHANTABLE.get()) return;
+        if (CommonConfig.ATOMIC_DISASSEMBLER_ALLOW_ALL_ENCHANTMENTS.get())
+            cir.setReturnValue(true);
+
+        if (!enchantable_mekatools$ALLOWED.contains(enchantment.category)) return;
+        if (enchantable_mekatools$DENIED.contains(enchantment)) return;
+        cir.setReturnValue(true);
+    }
+
+    @Override
+    protected void getEnchantmentValue(CallbackInfoReturnable<Integer> cir) {
+        if (!CommonConfig.ATOMIC_DISASSEMBLER_ENCHANTABLE.get()) return;
+        cir.setReturnValue(CommonConfig.ATOMIC_DISASSEMBLER_ENCHANTABILITY.get());
+    }
+}
