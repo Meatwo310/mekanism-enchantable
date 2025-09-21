2bec824165031030d9cc7b2e0060f3e509a4d7dc
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-09-21 12:37:37 +0900

    forge-47.4.x対応

--- src/main/java/io/github/meatwo310/mekanism_enchantable/MekanismEnchantable.java
+++ src/main/java/io/github/meatwo310/mekanism_enchantable/MekanismEnchantable.java
@@ -1,15 +1,15 @@
 package io.github.meatwo310.mekanism_enchantable;
 
 import io.github.meatwo310.mekanism_enchantable.config.CommonConfig;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(MekanismEnchantable.MODID)
 public class MekanismEnchantable {
     public static final String MODID = "mekanism_enchantable";
 
-    public MekanismEnchantable() {
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CommonConfig.SPEC);
+    public MekanismEnchantable(FMLJavaModLoadingContext ctx) {
+        ctx.registerConfig(ModConfig.Type.COMMON, CommonConfig.SPEC);
     }
 }
